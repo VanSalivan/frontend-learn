058b359c42275d4771f1bf45a8723320f6796ae5
aleksandr akelev <aleksandr.akelev@simbirsoft.com>
2021-03-11 15:59:09 +0300

    99. useEffect()

---
+++ Проекты/hooks/src/use-effect.js
@@ -0,0 +1,52 @@
+import { Component, useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    const [value, setValue] = useState(0);
+    const [visible, setVisible] = useState(true);
+
+    if (visible) {
+        return (
+            <div>
+                <button onClick={() => setValue((v) => v + 1)}>+</button>
+                <button onClick={() => setVisible(false)}>hide</button>
+                <ClassCounter value={value} />
+                <HookCounter value={value} />
+            </div>
+        )
+    } else {
+        return <button onClick={() => setVisible(true)}>show</button>
+    }
+};
+
+const HookCounter = ({ value }) => {
+
+    useEffect(() => {
+        console.log('useEffect()')
+
+        return () => console.log('clear') // при возврате функции очищает предыдущий эффект
+
+    }, [value])
+
+    return <p>{value}</p>
+};
+
+class ClassCounter extends Component {
+    componentDidMount() {
+        console.log("class: mount")
+    };
+
+    componentDidUpdate(props) {
+        console.log('class: update')
+    };
+
+    componentWillUnmount() {
+        console.log('class: unmount')
+    };
+
+    render() {
+        return <p>{this.props.value}</p>
+    };
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
