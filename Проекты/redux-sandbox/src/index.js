f4801108402bbd4e2b2460b2d7f771fcc010ddf0
aleksandr akelev <aleksandr.akelev@simbirsoft.com>
2021-03-16 11:42:31 +0300

    117. Reducer

--- Проекты/redux-sandbox/src/index.js
+++ Проекты/redux-sandbox/src/index.js
@@ -1,1 +1,22 @@
-console.log('Пустой проект');
+// Функция Reducer принимает стейт и обьект с типой события
+// state - текущее состояние, action - обьект с типом совершаемого действия
+const reducer = (state = 0, action) => {
+
+    switch (action.type) { // Проверяет тип события и выпоняет действия с состоянием/state
+        case 'INC':
+            return state + 1;
+
+        default:
+            return state //Если тип события не был найдет - вернет состояние в том виде в котором мы его получили
+    };
+};
+
+let state = reducer(undefined, {});
+console.log(state)
+
+// передаем в state предыдущий результат вычислений
+state = reducer(state, { type: 'INC' });
+console.log(state)
+
+state = reducer(state, { type: 'INC' });
+console.log(state)
