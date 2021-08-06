db52b860f914e8b7c3f78e4e4aac5bac933633f6
aleksandr akelev <aleksandr.akelev@simbirsoft.com>
2021-08-06 16:15:58 +0300

    RR_INTERNET-SHOP: 2. Настройка Redux/Connect-router

--- Учебные_проекты/React-Redux__internet-shop/src/index.js
+++ Учебные_проекты/React-Redux__internet-shop/src/index.js
@@ -1,3 +1,32 @@
+// Dependencies
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux'
+import { createBrowserHistory } from 'history'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import { ConnectedRouter, routerMiddleware } from 'connected-react-router'
+
+// Externals
+import createRootReducer from 'reducers'
+import Layout from 'containers/layout'
 import './main.css'
 
-document.write('Hello React/Redux!')
+
+const history = createBrowserHistory()
+const middlewares = [thunk, routerMiddleware(history)]
+const store = createStore(
+  createRootReducer(history),
+  composeWithDevTools(applyMiddleware(...middlewares))
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Layout />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+)
+
